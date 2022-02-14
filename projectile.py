21015743720339c60fc166e3304245b63313b394
Arthur <arthur.mans@insa-cvl.fr>
2022-02-14 23:32:56 +0100

    projectile placé au bon endroit selon le camp du combattant

--- projectile.py
+++ projectile.py
@@ -5,14 +5,16 @@ class Projectile(pygame.sprite.Sprite):
     def __init__(self, combattant):
         super().__init__()
         self.velocity = 5
+
+        #position du projectile
         self.image = pygame.image.load('assets/bdf.png')
         self.image = pygame.transform.scale(self.image, (100,100))
         self.rect = self.image.get_rect()
-        self.largeur_image = self.image.get_width()
-        self.hauteur_image = self.image.get_height()
+        self.largeur_image_combattant = combattant.image.get_width()
+        self.hauteur_image_combattant = combattant.image.get_height()
         if combattant.est_allié:
-            self.rect.x = combattant.rect.x 
-            self.rect.y = combattant.rect.y
+            self.rect.x = combattant.rect.x + self.largeur_image_combattant
+            self.rect.y = combattant.rect.y + (self.hauteur_image_combattant / 3)
         else:
-            self.rect.x = combattant.rect.x
-            self.rect.y = combattant.rect.y
+            self.rect.x = combattant.rect.x - self.image.get_width()
+            self.rect.y = combattant.rect.y + (self.hauteur_image_combattant / 3)
