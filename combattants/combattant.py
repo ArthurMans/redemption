c7087a8a1ba82a0303f6088c905d18f8d2ea6214
Antoine <antoine.lascombes@insa-cvl.fr>
2022-02-14 18:56:32 +0100

    Deplacement du joueur dans la fenetre

--- combattants/combattant.py
+++ combattants/combattant.py
@@ -2,11 +2,14 @@ import pygame
 
 class Combattant(pygame.sprite.Sprite):
 
-    def __init__(self, pv, nom):
+    def __init__(self, pv=100, nom='Antoine'):
         self.pv = pv
         self.nom = nom
         self.image = pygame.image.load('assets/player.png')
         self.rect = self.image.get_rect()
+        self.rect.x = 450
+        self.rect.y = 200
+        self.velocity = 20
         #self.robustesse
         #self.puissance
         #self.endurance
@@ -16,3 +19,15 @@ class Combattant(pygame.sprite.Sprite):
 
     def afficher_combattant(self):
         print("nom :",self.nom,"||  pv :",self.pv)
+
+    def bouger_a_droite(self):
+        self.rect.x += self.velocity
+
+    def bouger_a_gauche(self):
+        self.rect.x -= self.velocity
+
+    def bouger_en_haut(self):
+        self.rect.y -= self.velocity
+
+    def bouger_en_bas(self):
+        self.rect.y += self.velocity
--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import pygame
 from combattants.combattant import Combattant
 from combattants.blob import Blob
+from jeu import Jeu
 
 pygame.init()
 
 pygame.display.set_caption("Redemption")
-pygame.display.set_mode((1080, 720))
+ecran = pygame.display.set_mode((1080, 720))
+arriere_plan = pygame.image.load('assets/highnoondarkstar.jpg')
+joueur = Combattant()
+jeu = Jeu()
+
 running = True
 
 while running:
+
+    ecran.blit(arriere_plan,(0,0))
+    ecran.blit(jeu.joueur.image,jeu.joueur.rect)
+    pygame.display.flip()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                jeu.joueur.bouger_a_droite()
+            elif event.key == pygame.K_LEFT:
+                jeu.joueur.bouger_a_gauche()
+            elif event.key == pygame.K_UP:
+                jeu.joueur.bouger_en_haut()
+            elif event.key == pygame.K_DOWN:
+                jeu.joueur.bouger_en_bas()
