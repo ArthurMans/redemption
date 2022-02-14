bf4ba91ec09611e66c6a780742ab5cdfc26cea1f
Arthur <arthur.mans@insa-cvl.fr>
2022-02-14 12:47:03 +0100

    creation combattant

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-redemption pd
+competence
+carte
+combattant
+joueur
+objet (inventaire)
+inventaire
+arbre de competences
+boutique
+forge ?
+combat
+IA
---
+++ combattant.py
@@ -0,0 +1,10 @@
+class Combattant:
+
+    def __init__(self):
+        self.pv = 100
+        #self.robustesse
+        #self.puissance
+        #self.endurance
+        #self.perception
+        #self.reactivite
+        #self.parade
