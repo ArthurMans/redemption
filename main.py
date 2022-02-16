5f985df24400ecde68732452c32ce782605d0965
Antoine <antoine.lascombes@insa-cvl.fr>
2022-02-16 20:56:40 +0100

    bouton attaque (non fonctionnel), arthur fait le disparaitre stp

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ timer = pygame.time.Clock()
 
 pygame.display.set_caption("Redemption")
 ecran = pygame.display.set_mode((largeur_ecran, hauteur_ecran))
+bouton_attaque = pygame.image.load('assets/attaque.jpg')
 arriere_plan = pygame.image.load('assets/highnoondarkstar.jpg')
 arriere_plan = pygame.transform.scale(arriere_plan, (largeur_ecran, hauteur_ecran))
 jeu = Jeu()
+
 
 running = True
 
@@ -69,7 +71,17 @@ while running:
                 if event.key == pygame.K_SPACE:
                     jeu.heros.lancer_projectile()
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                attaque_en_cours = True
-                dx, dy = jeu.heros.vitesse_deplacement(jeu.blob)
+                xmin = jeu.blob.rect.x
+                xmax = jeu.blob.rect.x + jeu.blob.rect.width
+                ymin = jeu.blob.rect.y
+                ymax = jeu.blob.rect.y + jeu.blob.rect.height
+                if xmin <= pygame.mouse.get_pos()[0] <= xmax and ymin <= pygame.mouse.get_pos()[1] <= ymax:
+                    arriere_plan.blit(bouton_attaque, (850,1000))
+                    pos_bouton_attaque = bouton_attaque.get_rect(topleft=(850,1000))
+                if pos_bouton_attaque[0] <= pygame.mouse.get_pos()[0] <= pos_bouton_attaque[0]+pos_bouton_attaque[2] and pos_bouton_attaque[1] <= pygame.mouse.get_pos()[1] <= pos_bouton_attaque[1]+pos_bouton_attaque[3]:
+                    arriere_plan.blit(bouton_attaque, (0,0))
+                    attaque_en_cours = True
+                    dx, dy = jeu.heros.vitesse_deplacement(jeu.blob)
 
     timer.tick(FPS)
+
